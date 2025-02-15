7545e79d468a3948b2bc11e810bf2f2c6545526b
Elsayed Hesham <elsayedheshamdarwish@gmail.com>
2025-02-15 17:53:19 +0200

    ,,,

--- bank_system/FileManger.h
+++ bank_system/FileManger.h
@@ -11,7 +11,7 @@ class FileManger :
     // i make static func to use the class without obj 
 public:
 
-   
+
 
     void static add_Employee(Employee person) {
         fstream file("Employee.txt");
@@ -40,141 +40,122 @@ public:
 
 
 
-   static void get_all_employee() {
+    static void get_all_employee() {
 
-       ifstream file("Employee.txt", ios::in);
-           string line;
-           if (!file) {
-               cout << "file not found ";
-               return;
-           }
+        ifstream file("Employee.txt", ios::in);
+        string line;
+        if (!file) {
+            cout << "file not found ";
+            return;
+        }
 
-           while (getline(file, line)){
+        while (getline(file, line)) {
 
-               cout << line << endl;
-           }
-        
-           file.close();
-   }
+            cout << line << endl;
+        }
 
-
-
-
-     
-       
-
-   
-
-
-
-
-  
-
-
-  
-
-
-
-    //add client to text file
-
-   static void  add_Client(Client person) {
-
-        ofstream file("client.txt", ios::app);
-        if (!file.good()) {
-            cout << "There was an error with the file." << endl;
-            exit(1);
-      }
-        file << person.getName()<< "|";
-        file << person.getId() << "|";
-        file << person.getBalance() << "|";
-        file << person.getPassword() << "\n";
         file.close();
 
-   }
-
-
-   static void  remove_all_Clients() {
-         ofstream file("client.txt",  ios::trunc);
-         if (!file) {
-             cout<<"There was an error with the file.";
-             exit(1);
-         }
-
-         file.close();
-       
-        
     }
 
-   static void get_all_clients() {
+        static void  add_Client(Client person) {
 
-       ifstream file("client.txt", ios::in);
-       if (!file) {
-           cout << "there was an erroe with file ";
-           exit(0);
-       }
-       ;
-       string line;
+            ofstream file("client.txt", ios::app);
+            if (!file.good()) {
+                cout << "There was an error with the file." << endl;
+                exit(1);
+            }
+            file << person.getName() << "|";
+            file << person.getId() << "|";
+            file << person.getBalance() << "|";
+            file << person.getPassword() << "\n";
+            file.close();
 
-
-       while (getline(file, line)) {
-
-           cout << line << endl;
-
-
-       }
-
-   }
-        
+        }
 
 
-   static void add_admin(Admin admin) {
+        static void  remove_all_Clients() {
+            ofstream file("client.txt", ios::trunc);
+            if (!file) {
+                cout << "There was an error with the file.";
+                exit(1);
+            }
 
-       ofstream file("admin.txt", ios::app);
-
-       if (!file) {
-           cout << "file not exist ";
-           exit(0);
-       }
-       file << admin.getName() <<"|";
-       file << admin.getId() << "|";
-       file << admin.getSalary() << "|";
-       file << admin.getPassword() << "\n";
-       file.close();
-   }
-  
-
-   static void delete_Admin() {
-
-       ofstream file("admin.txt", ios::trunc);
-       if (!file) {
-           cout<<"there was an erroe with file ";
-           return;
-
-       }
-       file.close();
-   }
+            file.close();
 
 
+        }
 
-   static void get_all_admin() {
+        static void get_all_clients() {
 
-       ifstream file("admin.txt", ios::in);
-       if (!file) {
-           cout << " can not find file";
-           exit(0);
-       }
-    
-       string line;
-       while (getline(file, line)) {
+            ifstream file("client.txt", ios::in);
+            if (!file) {
+                cout << "there was an erroe with file ";
+                exit(0);
+            }
+          
+            string line;
 
-           cout << line << endl;
 
-       }
+            while (getline(file, line)) {
 
-       file.close();
-     
+                cout << line << endl;
 
-   }
 
-};
+            }
+
+        }
+
+
+
+        static void add_admin(Admin admin) {
+
+            ofstream file("admin.txt", ios::app);
+
+            if (!file) {
+                cout << "file not exist ";
+                exit(0);
+            }
+            file << admin.getName() << "|";
+            file << admin.getId() << "|";
+            file << admin.getSalary() << "|";
+            file << admin.getPassword() << "\n";
+            file.close();
+        }
+
+
+        static void delete_Admin() {
+
+            ofstream file("admin.txt", ios::trunc);
+            if (!file) {
+                cout << "there was an erroe with file ";
+                return;
+
+            }
+            file.close();
+        }
+
+
+
+        static void get_all_admin() {
+
+            ifstream file("admin.txt", ios::in);
+            if (!file) {
+                cout << " can not find file";
+                exit(0);
+            }
+
+            string line;
+            while (getline(file, line)) {
+
+                cout << line << endl;
+
+            }
+
+            file.close();
+
+        }
+
+
+    };
 
