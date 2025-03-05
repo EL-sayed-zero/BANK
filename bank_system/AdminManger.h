19d5c92fe19754a46d94a2932b9057c339f126e9
A7med10Elbabry <ahmad1462004@gmail.com>
2025-03-05 02:43:47 +0200

    employee methods

--- bank_system/AdminManger.h
+++ bank_system/AdminManger.h
@@ -8,7 +8,10 @@ class AdminManger
 		cout << "2-clients list\n";
 		cout << "3-search client\n";
 		cout << "4-edit client\n";
-		cout << "5-logout\nenter your choise:";
+		cout << "5-Employee list\n";
+		cout << "6-add employee\n";
+		cout << "7-search employee\n";
+		cout << "10-logout\nenter your choise:";
 	}
 	Admin* login(int id, string password) {
 		Admin admin;
@@ -52,6 +55,27 @@ class AdminManger
 
 		}
 	}
+
+
+
+	void employee_list() {
+		Admin admin;
+		admin.employee_list();
+	}
+
+	void add_employee(Employee employee) {
+		Admin admin;
+		admin.addEmployee(employee);
+	}
+
+	Employee search_employee(int id) {
+		Admin admin;
+		return admin.searchemployee(id);
+	}
+
+
+
+
 
 
 };
